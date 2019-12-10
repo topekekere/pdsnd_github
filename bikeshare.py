624ab62f0849ac039be35e4290e675f85891c901
Temitope Kekere <topekekere@gmail.com>
2019-12-10 17:30:10 +0200

    Reworded print statementt

--- bikeshare.py
+++ bikeshare.py
@@ -30,7 +30,7 @@ def get_city():
         elif city.lower() == 'washington':
             return 'washington.csv'
         else:
-            print('Sorry, I do not understand your input. Please input either '
+            print('Sorry, please try again. Please input either '
                   'Chicago, New York, or Washington.')
 
 def get_time_period():
